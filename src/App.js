4efbd6db5ce0c6ab8ae0414f27ca44b865610b25
kelvin wekesa <kelvin@wekesa.kelvin>
2019-07-25 21:09:09 +0300

    make it better

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import AppBar from './appBar'
 import NestedGrid from './demo'
+import Progress from './progress'
 
 class App extends Component {
-  state = { characters: [ /*{ name: "relax" , job: "boss you too much" , image: "static/live-from-space.jpg" , market: "UK/London"}*/ ] };
+  state = {
+    characters: [ /*{ name: "relax" , job: "boss you too much" , image: "static/live-from-space.jpg" , market: "UK/London"}*/ ] ,
+    loaded: false ,
+  };
   componentDidMount ( ) {
     fetch ( "/datum" )
       .then ( result => result.json ( ) )
         .then ( result => {
           this.setState ( {
-            characters: [ ...this.state.characters , ...result ]
+            characters: [ ...this.state.characters , ...result ] ,
+            loaded: true ,
           } )
       } );
     }
@@ -18,8 +23,7 @@ class App extends Component {
       <div>
         <AppBar />
         <hr />
-        <NestedGrid elements={ this.state.characters }
-        />
+        {this.state.loaded ? <NestedGrid elements={ this.state.characters } /> : <Progress />}
       </div>
     );
  }
