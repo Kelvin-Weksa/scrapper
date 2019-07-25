8d160e12bf9f3c5db7ae41959b81f45271da10bb
kelvin wekesa <kelvin@wekesa.kelvin>
2019-07-25 13:35:53 +0300

    make it better

--- server.js
+++ server.js
@@ -13,40 +13,40 @@ app.get ( '/ping' , function ( req , res ) {
 });
 
 function run ( ) {
-    return new Promise ( async ( resolve , reject ) => {
-        try {
-          const browser = await puppeteer.launch ( { args: [ '--no-sandbox' , '--disable-setuid-sandbox' ] } );
-          const page = await browser.newPage ( );
-          await page.setRequestInterception ( true );
-          page.on ( 'request' , ( request ) => {
-            if ( request .resourceType ( ) === 'document' ) {
-              request .continue ( );
-            } else {
-              request.abort ( );
-            }
-          } );
-          await page.goto ( "https://www.steinigers.com/our-team/" , { timeout: 0 } );
-          let urls = await page.evaluate ( ( ) => {
-            let results = [ ];
-            let items2 = document .querySelectorAll ( 'div.k2UsersBlock.peoplemod' );
-            let items3 = Array.from ( items2 ) .map ( array => array .querySelectorAll ( 'td.pmt2' ) );
-            items3.forEach ( ( item ) => {
-              item.forEach ( ( element ) => {
-                results.push ( {
-                    name : element .querySelector ( 'a.UserName' ) .innerText ,
-                    job  : element .querySelector ( 'div.UserURL' ) .innerText ,
-                    image: "static/live-from-space.jpg" ,
-                } );
-              } )
-            } );
-            return results;
-          } )
-          browser.close ( );
-          return resolve ( urls );
-        } catch ( e ) {
-          return reject ( e );
+  return new Promise ( async ( resolve , reject ) => {
+    try {
+      const browser = await puppeteer.launch ( { args: [ '--no-sandbox' , '--disable-setuid-sandbox' ] } );
+      const page = await browser.newPage ( );
+      await page.setRequestInterception ( true );
+      page.on ( 'request' , ( request ) => {
+        if ( request .resourceType ( ) === 'document' ) {
+          request .continue ( );
+        } else {
+          request.abort ( );
         }
-    })
+      } );
+      await page.goto ( "https://www.steinigers.com/our-team/" , { timeout: 0 } );
+      let urls = await page.evaluate ( ( ) => {
+        let results = [ ];
+        let items2 = document .querySelectorAll ( 'div.k2UsersBlock.peoplemod' );
+        let items3 = Array.from ( items2 ) .map ( array => array .querySelectorAll ( 'td.pmt2' ) );
+        items3.forEach ( ( item ) => {
+          item.forEach ( ( element ) => {
+            results.push ( {
+                name : element .querySelector ( 'a.UserName' ) .innerText ,
+                job  : element .querySelector ( 'div.UserURL' ) .innerText ,
+                image: "static/live-from-space.jpg" ,
+            } );
+          } )
+        } );
+        return results;
+      } )
+      browser.close ( );
+      return resolve ( urls );
+    } catch ( e ) {
+      return reject ( e );
+    }
+  )
 }
 run ( ).then ( console.log ) .catch ( console.error );
 app.get ( '/datum' , function ( req , res ) {
