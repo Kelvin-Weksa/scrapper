0b34dad5c1e8d373d5b4d07c371167354027f0b4
kelvin wekesa <kelvin@wekesa.kelvin>
2019-08-13 21:19:23 +0300

    make it better

--- server.js
+++ server.js
@@ -5042,7 +5042,7 @@ function biogenerationventures ( ) {
             }
         } )
       }
-      let datas = await Promise.all ( [  ...urls .slice ( 0 , 8 ). map ( crawlUrl ) ] ) .catch ( e => { console.log ( e ) } );
+      let datas = await Promise.all ( [  ...urls .map ( crawlUrl ) ] ) .catch ( e => { console.log ( e ) } );
       //
       browser.close ( );
       return resolve ( [ ] .concat ( ...datas ) );
